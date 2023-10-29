751fc8d3adf4f466c2b33401c2825ef35cbe320d
lindseycarlson23 <144490541+lindseycarlson23@users.noreply.github.com>
2023-10-29 17:40:49 -0500

    added employees query

--- db/query.sql
+++ db/query.sql
@@ -1,12 +1,32 @@
 --view all departments
     --formatted table showing department names and department ids
 
---view all rows
-    --job title, role id, the department that role beongs to, and the salary for that role
+
+
+--view all roles
+    --job title, role id, the department that role belongs to, and the salary for that role
+USE t5nwm6np5lskt3ga;
+
+SELECT roles.title, roles.id, departments.department_name, roles.salary
+FROM roles
+LEFT JOIN departments
+ON roles.department_id = departments.id;
 
 --view all employees
     --formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-
+WITH managersCTE AS (SELECT * FROM employees WHERE manager_id IS NULL)
+SELECT 
+	e.id, 
+    e.first_name, 
+    e.last_name,
+	roles.title, 
+    departments.department_name AS department, 
+    roles.salary,
+    CONCAT(managersCTE.first_name, ' ', managersCTE.last_name) AS manager    
+FROM employees AS e
+LEFT JOIN roles ON e.role_id = roles.id
+LEFT JOIN departments ON roles.department_id = departments.id
+LEFT JOIN managersCTE ON e.manager_id = managersCTE.id
 --add a department
     --prompted to enter the name of the department and that department is added to the database
 
--- index.js
+++ index.js
@@ -43,15 +43,7 @@ function init() {
             }
         ])
         .then((answers) => {
-            // if(answers.action == "View Departments") {
-            //     // query for getting all department
-            //     // SELECT * FROM departments;
-            // } else if(answers.action == "View Roles") {
-            //     // SELECT * FROM roles;
-            // } else if(answers.action == "View Employees") {
-            //     // SELECT * FROM employees;
-            // }
-
+            
             switch(answers.action) {
                 case "View Departments":
                     // SELECT * FROM departments;
@@ -59,13 +51,21 @@ function init() {
                     break;
                 case "View Roles":
                     // SELECT * FROM roles;
-                    console.log("Showing roles!")
-
+                    viewAllRoles();
                     break;
-
-                case "Add Department":
-                    addDepartment();
+                case "View Employees":
+                    // SELECT * FROM roles;
+                    viewAllEmployees();
                     break;
+                // case "Add Department":
+                //     addDepartment();
+                //     break;
+                // case "Add Role":
+                //     addRole();
+                //     break;
+                // case "Add Employee":
+                //     addEmployee();
+                //     break;
                 case "Quit":
                     process.exit(1)
                 default:
@@ -74,51 +74,156 @@ function init() {
 
         })
     
-    }
+};
 
 // define all queries as their own functions
-    // export and import as needed
-
 function viewAllDepartments() {
     db.query("SELECT * FROM departments;", function(err, data) {
         if(err) {
             console.log(err)
             return;
         }
-
         console.table(data)
         init();
     })
-}
+};
 
-function addDepartment() {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "department_name",
-            message: "What is the name of the new department?"
+
+function viewAllRoles() {
+    db.query(`SELECT roles.title, roles.id, departments.department_name, roles.salary
+    FROM roles
+    LEFT JOIN departments
+    ON roles.department_id = departments.id;`, function(err, data) {
+        if(err) {
+            console.log(err)
+            return;
         }
-    ])
-    .then(answers => {
-        // INSERT INTO departments(department_name) VALUES (answers.department_name)
-
-        // db.query("INSERT INTO departments(department_name, more) VALUES (?, ?);", [answers.department_name, answers.more])
-
-        db.query("INSERT INTO departments(department_name) VALUES (?);", [answers.department_name], function(err, data) {
-            if(err) {
-                console.log(err)
-                return;
-            }
-            console.log("Department has been created!")
-            init();
-        })
-
+        console.table(data)
+        init();
     })
-}
+};
 
 
+function viewAllEmployees() {
+    db.query(`WITH managersCTE AS (SELECT * FROM employees WHERE manager_id IS NULL)
+    SELECT 
+        e.id, 
+        e.first_name, 
+        e.last_name,
+        roles.title, 
+        departments.department_name AS department, 
+        roles.salary,
+        CONCAT(managersCTE.first_name, ' ', managersCTE.last_name) AS manager    
+    FROM employees AS e
+    LEFT JOIN roles ON e.role_id = roles.id
+    LEFT JOIN departments ON roles.department_id = departments.id
+    LEFT JOIN managersCTE ON e.manager_id = managersCTE.id`, function(err, data) {
+        if(err) {
+            console.log(err)
+            return;
+        }
+        console.table(data)
+        init();
+    })
+};
 
 
+// function addDepartment() {
+//     inquirer.prompt([
+//         {
+//             type: "input",
+//             name: "department_name",
+//             message: "What is the name of the new department?"
+//         }
+//     ])
+//     .then(answers => {
+//         // INSERT INTO departments(department_name) VALUES (answers.department_name)
+
+//         // db.query("INSERT INTO departments(department_name, more) VALUES (?, ?);", [answers.department_name, answers.more])
+
+//         db.query("INSERT INTO departments(department_name) VALUES (?);", [answers.department_name], function(err, data) {
+//             if(err) {
+//                 console.log(err)
+//                 return;
+//             }
+//             console.log("Department has been created!")
+//             init();
+//         })
+
+//     })
+// };
+
+
+// function addRole() {
+//     inquirer.prompt([
+//         {
+//             type: "input",
+//             name: "title",
+//             message: "What is the role title?"
+//         },
+//         {
+//             type: "input",
+//             name: "salary",
+//             message: "What is the salary?"
+//         },
+//         {
+//             type: "input",
+//             name: "department_id",
+//             message: "What is the department id?"
+//         }
+//     ])
+//     .then(answers => {
+//         // INSERT INTO departments(department_name) VALUES (answers.department_name)
+
+//         // db.query("INSERT INTO departments(department_name, more) VALUES (?, ?);", [answers.department_name, answers.more])
+
+//         db.query("INSERT INTO roles(title, salary, department_id) VALUES (?, ?, ?);", [answers.title, answers.salary, answers.department_id], function(err, data) {
+//             if(err) {
+//                 console.log(err)
+//                 return;
+//             }
+//             console.log("New role has been created!")
+//             init();
+//         })
+
+//     })
+// };
+
+
+// function addEmployee() {
+//     inquirer.prompt([
+//         {
+//             type: "input",
+//             name: "first_name",
+//             message: "What is the first name?"
+//         },
+//         {
+//             type: "input",
+//             name: "last_name",
+//             message: "What is the last name?"
+//         },
+//         {
+//             type: "input",
+//             name: "role_id",
+//             message: "What is the role id?"
+//         }
+//     ])
+//     .then(answers => {
+//         // INSERT INTO departments(department_name) VALUES (answers.department_name)
+
+//         // db.query("INSERT INTO departments(department_name, more) VALUES (?, ?);", [answers.department_name, answers.more])
+
+//         db.query("INSERT INTO roles(title, salary, department_id) VALUES (?, ?, ?);", [answers.title, answers.salary, answers.department_id], function(err, data) {
+//             if(err) {
+//                 console.log(err)
+//                 return;
+//             }
+//             console.log("New role has been created!")
+//             init();
+//         })
+
+//     })
+// };
 
 
 init();
